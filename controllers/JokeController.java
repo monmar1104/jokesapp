85bbb79af8bfdea81be35e4340e3f6abfdcc1bfc
Marcin Pastwa <marcinpastwa@wp.pl>
2018-07-12 22:11:03 +0200

    jokes are displayed

--- controllers/JokeController.java
+++ controllers/JokeController.java
@@ -2,9 +2,11 @@ package com.monmar.jokes.jokeapp.controllers;
 
 import com.monmar.jokes.jokeapp.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JokeController {
 
     private JokeService jokeService;
